e737ca800fdcaec3abdb673eae78dc1769680b2e
cicanoctavian12-hue <cicanoctavian12@gmail.com>
2025-11-30 09:35:45 +0200

    Update .js

--- .js
+++ .js
@@ -1,11 +1,10 @@
- // Calling our requirements
+// Calling our requirements
 const {
   Client,
   GatewayIntentBits,
   REST,
   Routes,
   SlashCommandBuilder,
-  ActionRowBuilder,
   MessageFlags,
   TextDisplayBuilder,
   SeparatorBuilder,
@@ -29,29 +28,19 @@ const commands = [
     .setName('game')
     .setDescription('Send game info panel')
     .addStringOption(opt =>
-      opt.setName('phone_link')
-        .setDescription('Phone link')
-        .setRequired(true)
+      opt.setName('phone_link').setDescription('Phone link').setRequired(true)
     )
     .addStringOption(opt =>
-      opt.setName('pc_link')
-        .setDescription('PC link')
-        .setRequired(true)
+      opt.setName('pc_link').setDescription('PC link').setRequired(true)
     )
     .addStringOption(opt =>
-      opt.setName('loader_link')
-        .setDescription('Loader link')
-        .setRequired(true)
+      opt.setName('loader_link').setDescription('Loader link').setRequired(true)
     )
     .addStringOption(opt =>
-      opt.setName('sg_build_vers')
-        .setDescription('Stumble Guys build version')
-        .setRequired(true)
+      opt.setName('sg_build_vers').setDescription('Stumble Guys build version').setRequired(true)
     )
     .addStringOption(opt =>
-      opt.setName('stumble_hour_build_vers')
-        .setDescription('Stumble Hour build version')
-        .setRequired(true)
+      opt.setName('stumble_hour_build_vers').setDescription('Stumble Hour build version').setRequired(true)
     )
     .toJSON()
 ];
@@ -59,77 +48,96 @@ const commands = [
 // --------- COMMAND HANDLER ----------
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
+  if (interaction.commandName !== "game") return;
 
-  if (interaction.commandName === "game") {
-    const phone = interaction.options.getString("phone_link");
-    const pc = interaction.options.getString("pc_link");
-    const loader = interaction.options.getString("loader_link");
-    const sg = interaction.options.getString("sg_build_vers");
-    const sh = interaction.options.getString("stumble_hour_build_vers");
-    const now = Math.floor(Date.now() / 1000);
+  const phone = interaction.options.getString("phone_link");
+  const pc = interaction.options.getString("pc_link");
+  const loader = interaction.options.getString("loader_link");
+  const sg = interaction.options.getString("sg_build_vers");
+  const sh = interaction.options.getString("stumble_hour_build_vers");
+  const now = Math.floor(Date.now() / 1000);
 
-    const container = new ContainerBuilder()
-      .addTextDisplayComponents(
-        new TextDisplayBuilder().setContent("**Stumble Hour Downloads**")
+  const container = new ContainerBuilder()
+    // TITLE
+    .addTextDisplayComponents(
+      new TextDisplayBuilder().setContent("**Stumble Hour Downloads**")
+    )
+
+    // SEPARATOR
+    .addSeparatorComponents(
+      new SeparatorBuilder().setSpacing(SeparatorSpacingSize.Small).setDivider(true)
+    )
+
+    // INFO TEXT
+    .addTextDisplayComponents(
+      new TextDisplayBuilder().setContent(
+        `⏱️ **Last Updated:** <t:${now}:D>\n\n` +
+        `🔹 **Stumble Guys build version:** ${sg}\n\n` +
+        `🔸 **Stumble Hour version:** ${sh}`
       )
-      .addSeparatorComponents(
-        new SeparatorBuilder().setSpacing(SeparatorSpacingSize.Small).setDivider(true)
-      )
-      .addTextDisplayComponents(
-        new TextDisplayBuilder().setContent(
-          `⏱️ **Last Updated:** <t:${now}:D>\n\n` +
-          `🔹 **Stumble Guys build version:** ${sg}\n\n` +
-          `🔸 **Stumble Hour version:** ${sh}`
+    )
+
+    // SEPARATOR
+    .addSeparatorComponents(
+      new SeparatorBuilder().setSpacing(SeparatorSpacingSize.Small).setDivider(true)
+    )
+
+    // PHONE SECTION
+    .addSectionComponents(section =>
+      section
+        .addTextDisplayComponents(
+          new TextDisplayBuilder().setContent("📱 **Phone Download**\n<:BF:1442503402968842371> Android")
         )
-      )
-      .addSeparatorComponents(
-        new SeparatorBuilder().setSpacing(SeparatorSpacingSize.Small).setDivider(true)
-      )
-      .addTextDisplayComponents(
-        new TextDisplayBuilder().setContent("📱 Phone Download\n<:BF:1442503402968842371> Android")
-      )
-      .addActionRowComponents(
-        new ActionRowBuilder().addComponents(
+        .setButtonAccessory(
           new ButtonBuilder()
             .setStyle(ButtonStyle.Link)
             .setLabel("Android Download (APK)")
             .setURL(phone)
         )
-      )
-      .addSeparatorComponents(
-        new SeparatorBuilder().setSpacing(SeparatorSpacingSize.Small).setDivider(true)
-      )
-      .addTextDisplayComponents(
-        new TextDisplayBuilder().setContent("💻 PC Download\n<:BF:1442503402968842371> Game Folder")
-      )
-      .addActionRowComponents(
-        new ActionRowBuilder().addComponents(
+    )
+
+    // SEPARATOR
+    .addSeparatorComponents(
+      new SeparatorBuilder().setSpacing(SeparatorSpacingSize.Small).setDivider(true)
+    )
+
+    // PC SECTION
+    .addSectionComponents(section =>
+      section
+        .addTextDisplayComponents(
+          new TextDisplayBuilder().setContent("💻 **PC Download**\n<:BF:1442503402968842371> Game Folder")
+        )
+        .setButtonAccessory(
           new ButtonBuilder()
             .setStyle(ButtonStyle.Link)
             .setLabel("PC Download")
             .setURL(pc)
         )
-      )
-      .addSeparatorComponents(
-        new SeparatorBuilder().setSpacing(SeparatorSpacingSize.Small).setDivider(true)
-      )
-      .addTextDisplayComponents(
-        new TextDisplayBuilder().setContent("<:sg_check:1442516721658495056> Loader DLL\n<:BF:1442503402968842371> PC Loader")
-      )
-      .addActionRowComponents(
-        new ActionRowBuilder().addComponents(
+    )
+
+    // SEPARATOR
+    .addSeparatorComponents(
+      new SeparatorBuilder().setSpacing(SeparatorSpacingSize.Small).setDivider(true)
+    )
+
+    // LOADER SECTION
+    .addSectionComponents(section =>
+      section
+        .addTextDisplayComponents(
+          new TextDisplayBuilder().setContent("<:sg_check:1442516721658495056> **Loader DLL**\n<:BF:1442503402968842371> PC Loader")
+        )
+        .setButtonAccessory(
           new ButtonBuilder()
             .setStyle(ButtonStyle.Link)
             .setLabel("Loader Download")
             .setURL(loader)
         )
-      );
+    );
 
-    return interaction.reply({
-      components: [container],
-      flags: MessageFlags.IsComponentsV2
-    });
-  }
+  return interaction.reply({
+    components: [container],
+    flags: MessageFlags.IsComponentsV2
+  });
 });
 
 // --------- BOT LOGIN ----------
